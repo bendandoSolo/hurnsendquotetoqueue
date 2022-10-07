12b341063105fce23707388f417525e7c1b23142
ben roberts <bendando@hotmail.co.uk>
2022-10-07 12:31:12 +0100

    message stored to queue

--- sendhurnquoterequesttoqueue.cs
+++ sendhurnquoterequesttoqueue.cs
@@ -16,16 +16,36 @@ namespace Bendando.SendHurnQuoteToQueue
         }
 
         [Function("sendhurnquoterequesttoqueue")]
-        public HttpResponseData Run([HttpTrigger(AuthorizationLevel.Anonymous, "get", "post")] HttpRequestData req)
+        public static MultiResponse Run([HttpTrigger(AuthorizationLevel.Function, "get", "post")] HttpRequestData req,
+        FunctionContext executionContext)
+    {
+        var logger = executionContext.GetLogger("HttpExample");
+        logger.LogInformation("C# HTTP trigger function processed a request.");
+
+        var message = "Welcome to Azure Functions!";
+
+        var response = req.CreateResponse(HttpStatusCode.OK);
+        response.Headers.Add("Content-Type", "text/plain; charset=utf-8");
+        response.WriteString(message);
+
+        // Return a response to both HTTP trigger and storage output binding.
+        return new MultiResponse()
         {
-            _logger.LogInformation("C# HTTP trigger function processed a request.");
+            // Write a single message.
+            Messages = new string[] { message },
+            HttpResponse = response
+        };
+		}
+	}
 
-            var response = req.CreateResponse(HttpStatusCode.OK);
-            response.Headers.Add("Content-Type", "text/plain; charset=utf-8");
+	public class MultiResponse
+	{
+    [QueueOutput("outqueue",Connection = "AzureWebJobsStorage")]
+    public string[] Messages { get; set; }
+    public HttpResponseData HttpResponse { get; set; }
+	}
 
-            response.WriteString("Welcome to Azure Functions!");
 
-            return response;
-        }
-    }
+
+
 }
